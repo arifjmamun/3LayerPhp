c32cc800ffbf9b2207d53d72d2e1c7267ed24e18
MD. ARIFUL ISLAM <arifjmamun24@gmail.com>
2017-03-06 19:52:16 +0600

    Removed unnecessary files

--- data.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$persons = [
-    ["name"=>"Ariful Islam", "age"=>23, "phone"=>"01832264272", "email"=>"arif@gmail.com"],
-    ["name"=>"Shariful Islam", "age"=>19, "phone"=>"0183888888", "email"=>"sharif@gmail.com"],
-    ["name"=>"Saiful Islam", "age"=>28, "phone"=>"01832264272", "email"=>"saiful@gmail.com"],
-    ["name"=>"Sadia", "age"=>8, "phone"=>"", "email"=>"sadia@gmail.com"],
-];
